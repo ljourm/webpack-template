cdd4ffed8acc37654ada83bab39d260f59af5b09
ljourm <ljourm@gmail.com>
2020-12-16 04:25:34 +0900

    feat: stylelintの導入と適用

---
+++ .stylelintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "plugins": [
+    "stylelint-scss",
+  ],
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+  ],
+  "rules": {
+    "scss/dollar-variable-colon-space-after": "always",
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "no-empty-source": null,
+    "no-descending-specificity": null,
+  },
+  "ignoreFiles": [
+    "dist/**",
+  ],
+}
