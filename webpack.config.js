42a7967d81903363723facf0f5354f6b5d88f5cf
ljourm <ljourm@gmail.com>
2020-12-16 03:36:28 +0900

    feat: scssの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const mode = "development"
+const enabledSourceMap = true;
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -17,6 +21,33 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: enabledSourceMap
+            },
+          },
+        ],
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/*", "!.keep", "!css"],
+    }),
+  ]
 };
