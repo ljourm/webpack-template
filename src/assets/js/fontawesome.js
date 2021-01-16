260fbda2f4ee3ee2b99ccad276357e05a7433c00
ljourm <ljourm@gmail.com>
2021-01-17 05:17:44 +0900

    feat: ヘッダーの作り込み

--- src/assets/js/fontawesome.js
+++ src/assets/js/fontawesome.js
@@ -1,5 +1,5 @@
 import {library, dom} from "@fortawesome/fontawesome-svg-core"
-import {faBookmark, faBars, faEnvelope} from "@fortawesome/free-solid-svg-icons"
+import {faBookmark, faBars, faTimes, faEnvelope} from "@fortawesome/free-solid-svg-icons"
 
-library.add(faBookmark, faBars, faEnvelope)
+library.add(faBookmark, faBars, faTimes, faEnvelope)
 dom.i2svg()
--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,16 +1,40 @@
 import $ from "jquery"
 
+const navHeight = 74
+const classActive = "is-active"
+const classFixed = "is-fixed"
+
 $(window).on("scroll", () => {
+  if($("body").hasClass(classFixed)) {
+    return
+  }
+
   const scrollTop = $(window).scrollTop()
   const fixedNavbar = $(".navbar.is-fixed-top")
 
-  if (scrollTop > 74) {
-    fixedNavbar.addClass("is-active")
+  if (scrollTop > navHeight) {
+    fixedNavbar.addClass(classActive)
   } else {
-    fixedNavbar.removeClass("is-active")
+    fixedNavbar.removeClass(classActive)
   }
 })
 
-$(".navbar-burger").on("click", () => {
-  $(".navbar-menu").toggleClass("is-active")
+$(".navbar-burger").on("click", function() {
+  const navButton = $(this)
+  navButton.toggleClass(classActive)
+
+  const navMenu = $(this).closest("nav")
+                         .find(".navbar-menu")
+  navMenu.toggleClass(classActive)
+
+  if (navMenu.hasClass(classActive)) {
+    const scrollTop = $(window).scrollTop()
+    $("body").addClass(classFixed)
+             .css({"top": -scrollTop})
+  } else {
+    const positionTopWithPx = $("body").css("top")
+    const scrollTop = positionTopWithPx.match(/\d+/)[0]
+    $("body").removeClass(classFixed)
+    $(window).scrollTop(scrollTop)
+  }
 })
